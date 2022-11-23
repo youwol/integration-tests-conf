d07da5f9ee65f65d8f978d56e4681d385feb2780
Julien DÉCHARNE <jdecharne@youwol.com>
2022-11-23 15:02:35 +0100

    👽️ use new py-youwol configuration API

--- yw_config_IT.py
+++ yw_config_IT.py
@@ -1,18 +1,20 @@
 import os
 import shutil
 from pathlib import Path
+
 import brotli
 from starlette.middleware.base import RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
 
-from youwol.configuration.models_config import Projects
+from youwol.configuration.models_config import Projects, RemoteConfig, DirectAuthUser
 from youwol.environment.config_from_module import IConfigurationFactory, Configuration
 from youwol.environment.youwol_environment import YouwolEnvironment
+from youwol.main_args import MainArguments
 from youwol.middlewares.models_dispatch import AbstractDispatch
 from youwol.routers.custom_commands.models import Command
+from youwol_utils.clients.oidc.oidc_config import PublicClient
 from youwol_utils.context import Context
-from youwol.main_args import MainArguments
 from youwol_utils.request_info_factory import url_match
 
 
@@ -32,7 +34,7 @@ class BrotliDecompress(AbstractDispatch):
 
     async def apply(self, incoming_request: Request, call_next: RequestResponseEndpoint, context: Context):
 
-        async with context.start(action="Apply BrotliDecompress") as ctx: # type: Context
+        async with context.start(action="Apply BrotliDecompress") as ctx:  # type: Context
             match_cdn, params = url_match(incoming_request, "GET:/api/assets-gateway/raw/package/*/**")
             await ctx.info(text="parameters", data={"matchCdn": match_cdn, "params": params})
 
@@ -60,8 +62,25 @@ class BrotliDecompress(AbstractDispatch):
 class ConfigurationFactory(IConfigurationFactory):
 
     async def get(self, main_args: MainArguments) -> Configuration:
+        username = os.getenv("USERNAME_INTEGRATION_TESTS")
+        password = os.getenv("PASSWORD_INTEGRATION_TESTS")
+        username_bis = os.getenv("USERNAME_INTEGRATION_TESTS_BIS")
+        password_bis = os.getenv("PASSWORD_INTEGRATION_TESTS_BIS")
         return Configuration(
             httpPort=2001,
+            remotes=[
+                RemoteConfig.build(
+                    default_user=username,
+                    direct_auth_users=[
+                        DirectAuthUser(username=username, password=password),
+                        DirectAuthUser(username=username_bis, password=password_bis)
+                        ],
+                    openid_client=PublicClient(
+                        client_id="tbd_test_openid_connect_js"
+                    ),
+                    host="platform.youwol.com"
+                )
+            ],
             dataDir=Path(__file__).parent / 'databases',
             cacheDir=Path(__file__).parent / 'youwol_system',
             projects=Projects(
